3a0887e8b365bd3ddde2767a17b7b2cba0f9c99c
arorashivoy <85881270+arorashivoy@users.noreply.github.com>
2021-07-06 15:31:19 +0530

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by Shivoy Arora on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
